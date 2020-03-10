bb0a7b923cb3e768c8d3979a2b4146dbeb146257
Adrian Kieblesz <adriano.kieblesz@gmail.com>
2020-03-10 16:40:55 +0100

    Initial test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+import Navigation from './components/JS/Navigation';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Navigation />
     </div>
   );
 }
---
+++ src/components/JS/Navigation.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import '../SCSS/Navigation.scss';
+
+class Navigation extends Component {
+    state = {}
+    render() {
+        return (
+            <nav>
+                <a href="">Test</a>
+                <a href="">Test</a>
+                <a href="">Test</a>
+                <a href="">Test</a>
+                <a href="">Test</a>
+            </nav>
+        );
+    }
+}
+
+export default Navigation;
+
