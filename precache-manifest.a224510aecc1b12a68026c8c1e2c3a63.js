d843014a5503bd77f2c75e969cde42412c2bfe51
Adrian Kieblesz <adriano.kieblesz@gmail.com>
2020-03-28 10:36:37 +0100

    Updates

---
+++ precache-manifest.a224510aecc1b12a68026c8c1e2c3a63.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c83e091c7a7cdb436610394bf975735a",
+    "url": "/module-fences-shop/index.html"
+  },
+  {
+    "revision": "b2ba6747d43b88df3987",
+    "url": "/module-fences-shop/static/css/main.dfd42d17.chunk.css"
+  },
+  {
+    "revision": "33c64b9e766fe5f1417a",
+    "url": "/module-fences-shop/static/js/2.0fe21567.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/module-fences-shop/static/js/2.0fe21567.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b2ba6747d43b88df3987",
+    "url": "/module-fences-shop/static/js/main.2a01df03.chunk.js"
+  },
+  {
+    "revision": "9a11a7381144de07afa8",
+    "url": "/module-fences-shop/static/js/runtime-main.5053dd35.js"
+  },
+  {
+    "revision": "0433220f40b1e265fe208bb22f08c493",
+    "url": "/module-fences-shop/static/media/background_1_flipped.0433220f.jpg"
+  }
+]);
