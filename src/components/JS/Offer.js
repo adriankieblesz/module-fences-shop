e0febe1c8c4035d343d33aa64739cb49a4c33593
Adrian Kieblesz <adriano.kieblesz@gmail.com>
2020-05-03 21:28:41 +0200

    Optimalization of offer section and new animations for product

--- src/components/JS/Offer.js
+++ src/components/JS/Offer.js
@@ -2,16 +2,38 @@ import React, { Component } from 'react';
 import OfferGrid from './OfferGrid';
 import OfferForm from './OfferForm';
 import '../SCSS/Offer.scss';
+class Offer extends Component {
+    state = {
+        products: []
+    }
+    componentDidMount() {
+        let products = [];
+        for (let i = 0; i < 9; i++) {
+            products.push({
+                id: i + 1,
+                url: require(`../../images/fence-models/type-${i + 1}.png`),
+                name: `Type ${i + 1}`,
+                price: (100 + 10 * i),
+                buyClick: this.handleBuyClick
+            });
+        }
+        this.setState({ products })
+        console.log(this.state.products)
+    }
+    handleBuyClick = (id) => {
+        alert(`Id of this product ${id}`);
+    }
+    render() {
 
-const Offer = () => {
-    return (
-        <section id="offer">
-            <h1>Offer</h1>
-            <p className="offer-description">In our extensive offer you can find everything according to your needs. We offer fences made of two types of materials (metal, wood) in various style variants.</p>
-            <OfferForm />
-            <OfferGrid />
-        </section>
-    );
+        return (
+            <section id="offer">
+                <h1>Offer</h1>
+                <p className="offer-description">In our extensive offer you can find everything according to your needs. We offer fences made of two types of materials (metal, wood) in various style variants.</p>
+                <OfferForm />
+                <OfferGrid products={this.state.products} />
+            </section>
+        );
+    }
 }
 
 export default Offer;
--- src/components/JS/Product.js
+++ src/components/JS/Product.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import buy from '../../images/icons/buy.png';
+import info from '../../images/icons/info30.png';
 import '../SCSS/Product.scss';
 
-const Product = () => {
+const Product = ({ url, name, price, buyClick }) => {
     return (
         <div className="product">
-
+            <img className="product-image" src={url} alt="" />
+            <h4>{name}</h4>
+            <div className="product-inner">
+                <p className="product-price">Price: {price}$</p>
+                <img className="product-info" src={info} alt="" title="info" />
+                <img className="product-buy" src={buy} alt="" onClick={buyClick} />
+            </div>
         </div>
     );
 }
